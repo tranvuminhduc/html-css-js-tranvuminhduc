87563955401e01eee7f7aa9b89bf52aecf34da9d
tranvuminhduc <aquamidu1@gmail.com>
2022-11-26 17:31:16 +0700

    BTVN 26/11

---
+++ HTML-JS/script.js
@@ -0,0 +1,34 @@
+// Bai tap 1
+function tongBaSo(a, b, c){
+    return (a+b+c);
+}
+
+// Bai tap 2
+function tenDayDu(ho, tenDem, ten){
+    return (ho + tenDem + ten);
+}
+
+// Bai tap 3
+function timSoDu(a, b){
+    return (a%b);
+}
+
+// Bai tap 4
+function timHieu3So(a, b, c){
+    return( a - b - x);
+}
+
+// Bai tap 5
+function tinhTrungBinhCong(a, b, c){
+    return ((a + b + c) / 3);
+}
+
+// Bai tap 6
+function tinhBieuThuc(a, b, c){
+    return (Math.round(a * 2 + (b / 3) / (c - 12)) / 10);
+}
+
+// Bai tap 7
+function tinhPhanTram(a) {
+    return (0.72*a);
+}
